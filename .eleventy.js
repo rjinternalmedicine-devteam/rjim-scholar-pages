049e18770a7b00536eff052ff0aba8f1640ad9be
rjinternalmedicine-devteam <rjinternalmedicine@gmail.com>
2025-08-29 20:53:48 -0400

    Eleventy update for robots and sitemap files

--- .eleventy.js
+++ .eleventy.js
@@ -1,12 +1,15 @@
 module.exports = function(eleventyConfig) {
-    eleventyConfig.addCollection("articles", (collectionApi) => {
-      return collectionApi.getFilteredByGlob("articles/*.md");
-    });
-  
-    eleventyConfig.addPassthroughCopy("CNAME");
-  
-    return {
-      dir: { input: ".", includes: "_includes", output: "_site" }
-    };
+  // collections
+  eleventyConfig.addCollection("articles", (collectionApi) => {
+    return collectionApi.getFilteredByGlob("articles/*.md");
+  });
+
+  // passthrough files that must exist at the site root
+  eleventyConfig.addPassthroughCopy("CNAME");
+  eleventyConfig.addPassthroughCopy("robots.txt");
+  eleventyConfig.addPassthroughCopy("sitemap.xml");
+
+  return {
+    dir: { input: ".", includes: "_includes", output: "_site" }
   };
-  
+};
